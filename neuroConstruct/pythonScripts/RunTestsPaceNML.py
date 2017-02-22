73d2967263090de67eff091bbfd825cc9292fe9d
Padraig Gleeson <padraig@Padraigs-MacBook-Pro.local>
2017-02-22 18:30:49 +0000

    Adding more tests on nml/nc versions

---
+++ neuroConstruct/pythonScripts/RunTestsPaceNML.py
@@ -0,0 +1,92 @@
+#
+#
+#   File to test current configuration of GranuleCell project.
+#
+#   To execute this type of file, type '..\..\..\nC.bat -python XXX.py' (Windows)
+#   or '../../../nC.sh -python XXX.py' (Linux/Mac). Note: you may have to update the
+#   NC_HOME and NC_MAX_MEMORY variables in nC.bat/nC.sh
+#
+#   Author: Padraig Gleeson
+#
+#   This file has been developed as part of the neuroConstruct project
+#   This work has been funded by the Medical Research Council and the
+#   Wellcome Trust
+#
+#
+
+import sys
+import os
+
+try:
+    from java.io import File
+except ImportError:
+    print "Note: this file should be run using ..\\..\\..\\nC.bat -python XXX.py' or '../../../nC.sh -python XXX.py'"
+    print "See http://www.neuroconstruct.org/docs/python.html for more details"
+    quit()
+
+sys.path.append(os.environ["NC_HOME"]+"/pythonNeuroML/nCUtils")
+
+import ncutils as nc
+
+projFile = File(os.getcwd(), "../VervaekeEtAl-GolgiCellNetwork.ncx")
+
+
+##############  Main settings  ##################
+
+simConfigs = []
+
+simConfigs.append("PacemakingNML")
+
+simDt =                 0.0025
+
+simulators =            ["NEURON"]
+
+varTimestepNeuron =     False
+
+plotSims =              True
+plotVoltageOnly =       True
+runInBackground =       True
+analyseSims =           True
+verbose =               False
+
+#############################################
+
+
+def testAll(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    print "Loading project from "+ projFile.getCanonicalPath()
+
+
+    simManager = nc.SimulationManager(projFile,
+                                      verbose = verbose)
+
+    simManager.runMultipleSims(simConfigs =           simConfigs,
+                               simDt =                simDt,
+                               simulators =           simulators,
+                               runInBackground =      runInBackground,
+                               varTimestepNeuron =    varTimestepNeuron)
+
+    simManager.reloadSims(plotVoltageOnly =   plotVoltageOnly,
+                          plotSims =          plotSims,
+                          analyseSims =       analyseSims)
+
+    # These were discovered using analyseSims = True above.
+    # They need to hold for all simulators
+    spikeTimesToCheck = {'CellGroup_3_0': [28.4025, 160.725, 300.38, 440.09, 579.33, 718.09, 856.445, 994.473, 1132.23, 1269.79, 1407.16, 1544.39, 1681.5, 1818.49, 1955.39]}
+    
+    spikeTimeAccuracy = 0.0
+
+    report = simManager.checkSims(spikeTimesToCheck = spikeTimesToCheck,
+                                  spikeTimeAccuracy = spikeTimeAccuracy)
+
+    print report
+
+    return report
+
+
+if __name__ == "__main__":
+    testAll()
+
+
